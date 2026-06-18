9328dfdf6957cbb3d0b3385282a67cffaa5c7590
henriksadiqrassmussen <63313954+henriksadiqrassmussen@users.noreply.github.com>
2026-06-18 22:44:01 +0200

    Delete compliance directory

--- compliance/oioubl-cli.js
+++
@@ -1,3 +0,0 @@
-
-const fs=require('fs'); const {exportOioUbl}=require('./oioubl');
-const inv=JSON.parse(fs.readFileSync(process.argv[2],'utf8')); console.log('OIOUBL written:', exportOioUbl(inv,process.argv[3]||'exports/invoice.xml'));
